4428a881c6f2db769080c974a4592cdc6566c8b5
Rudy Castan <rudy.castan@digipen.edu>
2021-02-17 16:36:25 +0900

    Updated docs for v0.2.5

--- 0.2/search/all_2.js
+++ 0.2/search/all_2.js
@@ -4,7 +4,7 @@ var searchData=
   ['backslash_20',['Backslash',['../namespacedoodle.html#ac5fa42d49165623a305f7ccd5607656eaf6c6379402dce27659f7cffee6bc1f00',1,'doodle']]],
   ['backspace_21',['Backspace',['../namespacedoodle.html#ac5fa42d49165623a305f7ccd5607656eacd7d13ceea728b08555f7c818cfb13ef',1,'doodle']]],
   ['begin_22',['begin',['../classdoodle_1_1_image.html#a4530a53fb2208f6dad6b8e133ae49852',1,'doodle::Image::begin()'],['../classdoodle_1_1_image.html#a868d5ee730c08cb570c237cc03846405',1,'doodle::Image::begin() const']]],
-  ['begin_5fdrawing_5fto_5fimage_23',['begin_drawing_to_image',['../group___image.html#ga5846e12b62d2ddd99a1d5309a4974574',1,'doodle']]],
+  ['begin_5fdrawing_5fto_5fimage_23',['begin_drawing_to_image',['../group___image.html#gade99b48af7186dec762060f576ccc196',1,'doodle']]],
   ['blue_24',['blue',['../structdoodle_1_1_color.html#aa2024130b1cfac205d9e3ab3875296cc',1,'doodle::Color']]],
   ['bmp_25',['BMP',['../classdoodle_1_1_image.html#a3ac3e1e8265fbc309086e7208a24eff8aa5d5ca1447586e23dc011f8c0cc0a6db',1,'doodle::Image']]]
 ];
--- 0.2/search/functions_1.js
+++ 0.2/search/functions_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['begin_260',['begin',['../classdoodle_1_1_image.html#a4530a53fb2208f6dad6b8e133ae49852',1,'doodle::Image::begin()'],['../classdoodle_1_1_image.html#a868d5ee730c08cb570c237cc03846405',1,'doodle::Image::begin() const']]],
-  ['begin_5fdrawing_5fto_5fimage_261',['begin_drawing_to_image',['../group___image.html#ga5846e12b62d2ddd99a1d5309a4974574',1,'doodle']]]
+  ['begin_5fdrawing_5fto_5fimage_261',['begin_drawing_to_image',['../group___image.html#gade99b48af7186dec762060f576ccc196',1,'doodle']]]
 ];
